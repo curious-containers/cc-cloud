b9c23731e8360e1994681df67243a0a751566069
Dominik Probst <Dominik.Probst@htw-berlin.de>
2023-05-08 14:40:57 +0200

    create filesystem manager class

---
+++ cc_cloud/filesystem_manager.py
@@ -0,0 +1,24 @@
+
+
+class FilesystemManager:
+    
+    def __init__(self, conf):
+        pass
+    
+    def create(self):
+        pass
+    
+    def delete(self):
+        pass
+    
+    def mount(self):
+        pass
+    
+    def umount(self):
+        pass
+    
+    def increse_size(self):
+        pass
+    
+    def reduce_size(self):
+        pass

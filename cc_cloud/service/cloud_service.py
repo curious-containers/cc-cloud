03f80bf139adac52ec72b24aae52784a866903db
Dominik Probst <Dominik.Probst@htw-berlin.de>
2023-05-23 14:27:08 +0200

    add user functions to cloudservice

--- cc_cloud/service/cloud_service.py
+++ cc_cloud/service/cloud_service.py
@@ -15,10 +15,10 @@ class CloudService:
         self.file_service = FileService(conf)
         self.filesystem_service = FilesystemService(conf)
         self.home_dir = '/var/lib/cc_cloud/home'
-        self.startup()
+        self.mount_filesystems()
     
     
-    def startup(self):
+    def mount_filesystems(self):
         for fs in self.filesystem_service.find_all_filesystems():
             self.filesystem_service.exists_or_create(fs)
     
@@ -27,13 +27,18 @@ class CloudService:
         return self.user_prefix + '-' + user.username
     
     
-    ## for every user
-    
-    def file_action(self, user, func, *args):
+    def local_user_exists_or_create(self, user):
         user_ref = self.get_user_ref(user)
         local_user = LocalUser(user_ref, self.home_dir)
         if not local_user.exists():
             local_user.create()
+        return user_ref, local_user
+    
+    
+    ## cloud storage actions
+    
+    def file_action(self, user, func, *args):
+        user_ref, _ = self.local_user_exists_or_create(user)
         self.filesystem_service.exists_or_create(user_ref)
         return func(user_ref, *args)
     
@@ -50,14 +55,33 @@ class CloudService:
         return self.file_action(user, self.file_service.delete_file, path)
     
     
+    ## local user actions
+    
+    def set_local_user_authorized_key(self, user, pub_key):
+        _, local_user = self.local_user_exists_or_create(user)
+        local_user.set_authorized_key(pub_key)
+    
+    
     ## only for admin users
     
     def create_user():
         pass
     
     
-    def remove_user():
-        pass
+    def remove_user(self, user, remove_user):
+        if not user.is_admin:
+            return False
+        
+        user_ref = self.get_user_ref(remove_user)
+        
+        self.filesystem_service.umount(user_ref)
+        self.filesystem_service.delete(user_ref)
+        
+        local_user = LocalUser(user_ref, self.home_dir)
+        if local_user.exists():
+            local_user.remove()
+            
+        return True
     
     
     

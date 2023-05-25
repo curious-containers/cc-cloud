61cdafe11625a1aa3344c3327742c38fa6af1053
Dominik Probst <Dominik.Probst@htw-berlin.de>
2023-05-25 11:26:18 +0200

    adding documentation

--- cc_cloud/service/cloud_service.py
+++ cc_cloud/service/cloud_service.py
@@ -12,6 +12,12 @@ class CloudService:
     user_prefix = 'cloud'
     
     def __init__(self, conf):
+        """Create a new instance of CloudService.
+        All existing file systems of the user are automatically mounted.
+
+        :param conf: Configuration to load values from
+        :type conf: cc_agency.commons.conf.Conf
+        """
         self.file_service = FileService(conf)
         self.filesystem_service = FilesystemService(conf)
         self.home_dir = '/var/lib/cc_cloud/home'
@@ -19,15 +25,31 @@ class CloudService:
     
     
     def mount_filesystems(self):
+        """Mount all existing file systems of the users.
+        """
         for fs in self.filesystem_service.find_all_filesystems():
             self.filesystem_service.exists_or_create(fs)
     
     
     def get_user_ref(self, user):
+        """Combines the username with a set prefix.
+
+        :param user: User for getting the reference.
+        :type user: cc_agency.broker.auth.Auth.User
+        :return: user reference
+        :rtype: str
+        """
         return self.user_prefix + '-' + user.username
     
     
     def local_user_exists_or_create(self, user):
+        """Check if the local user exists. If not create a new linux user.
+
+        :param user: create linux user based on the reference of this user
+        :type user: cc_agency.broker.auth.Auth.User
+        :return: user reference, instance of the created LocalUser
+        :rtype: str, cc_cloud.system.local_user.LocalUser
+        """
         user_ref = self.get_user_ref(user)
         local_user = LocalUser(user_ref, self.home_dir)
         if not local_user.exists():
@@ -38,26 +60,69 @@ class CloudService:
     ## cloud storage actions
     
     def file_action(self, user, func, *args):
+        """Check if the local user and the filesystem exists. If not create
+        the user and the filesystem. Then execute the given functions.
+
+        :param user: the user for whom the file action is executed
+        :type user: cc_agency.broker.auth.Auth.User
+        :param func: the function to be executed
+        :type func: callable
+        :return: result of the method func
+        """
         user_ref, _ = self.local_user_exists_or_create(user)
         self.filesystem_service.exists_or_create(user_ref)
         return func(user_ref, *args)
     
     
     def download_file(self, user, path):
+        """Checks if the user is allowed to access the file. If the path is available and
+        the user is allowed the access, the absolute filepath will be returned.
+
+        :param user: The user that wants to access the file
+        :type user: cc_agency.broker.auth.Auth.User
+        :param path: Path to the requested file
+        :type path: str
+        :return: Absolute filepath
+        :rtype: str
+        """
         return self.file_action(user, self.file_service.download_file, path)
     
     
     def upload_file(self, user, files):
-        return self.file_action(user, self.file_service.upload_file, files)
+        """Saves multiple files to the users storage.
+
+        :param user: The user that wants to upload the files
+        :type user: cc_agency.broker.auth.Auth.User
+        :param files: One or multiple files that should be saved
+        :type files: werkzeug.datastructures.structures.ImmutableMultiDict
+        """
+        self.file_action(user, self.file_service.upload_file, files)
     
     
     def delete_file(self, user, path):
+        """Deletes a file or directory from the given path.
+
+        :param user: The user that wants to delete an element
+        :type user: cc_agency.broker.auth.Auth.User
+        :param path: The path that will be deleted
+        :type path: str
+        :return: Returns True if the element was deleted
+        :rtype: bool
+        """
         return self.file_action(user, self.file_service.delete_file, path)
     
     
     ## local user actions
     
     def set_local_user_authorized_key(self, user, pub_key):
+        """Check if the user exists. If not create the user.
+        Then add the public ssh-key to the users authorized_keys file.
+
+        :param user: user for whom the ssh-key will be added
+        :type user: cc_agency.broker.auth.Auth.User
+        :param pub_key: public ssh-key, that will be added to the authorized_keys file
+        :type pub_key: str
+        """
         _, local_user = self.local_user_exists_or_create(user)
         local_user.set_authorized_key(pub_key)
     
@@ -69,6 +134,16 @@ class CloudService:
     
     
     def remove_user(self, user, remove_user):
+        """Delete the users (remove_user) filesystem and the local linux user.
+        The action will only be performed if user is admin. 
+
+        :param user: user who wants to perform the action
+        :type user: cc_agency.broker.auth.Auth.User
+        :param remove_user: the user which will be removed
+        :type remove_user: cc_agency.broker.auth.Auth.User
+        :return: if succeded returns true, otherwise false
+        :rtype: bool
+        """
         if not user.is_admin:
             return False
         
